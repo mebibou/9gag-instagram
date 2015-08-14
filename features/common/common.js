c389e9fdb6713bb404ce91d71eb3716a08f2b21c
groyer <guillaumeroyer.mail@gmail.com>
2015-08-14 15:59:00 +0200

    feat(posts): add infinite scroll and posts layout

--- features/common/common.js
+++ features/common/common.js
@@ -1,4 +1,87 @@
 (function() {
   'use strict';
 
+  function Common() {
+
+    window.EventsManager.call(this);
+
+    var _this = this,
+        _sort = '',
+        _$el = {
+          container: $('.container'),
+          sort: $('.sort-dropdown')
+        };
+
+    function _elementSort($element) {
+      return $element.attr('href').substring(1);
+    }
+
+    this.sort = function(sort) {
+      if (typeof sort != 'undefined') {
+        _sort = sort;
+        _this.fire('sortChanged', {
+          sort: sort
+        });
+      }
+
+      return _sort;
+    };
+
+    this.init = function() {
+      _this.sort(_elementSort(_$el.sort.find('li.active a')));
+
+      _$el.sort.find('.dropdown a').mousedown(function() {
+        _this.sort(_elementSort($(this)));
+      });
+    };
+
+    // Handle Infinite scroll
+
+    var _scrollTop = 0,
+        _maxHeight = _$el.container.height(),
+        _infineScrollingRequested = false,
+        _requestAnimationFrame = window.requestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            window.setTimeout;
+
+    function _refreshScroll() {
+      _scrollTop = $(window).scrollTop();
+    }
+    function _refreshMaxHeight() {
+      _maxHeight = _$el.container.height();
+    }
+    function _infiniteScroll() {
+      if (_infineScrollingRequested) {
+        return;
+      }
+
+      _infineScrollingRequested = true;
+      _requestAnimationFrame(function() {
+        _this.fire('infiniteScrollStart');
+      });
+    }
+
+    _this.on('infiniteScrollEnd', function() {
+      _infineScrollingRequested = false;
+      _refreshMaxHeight();
+    });
+
+    $(window).scroll(function() {
+      _refreshScroll();
+      if (_maxHeight - _scrollTop < 100) {
+        _infiniteScroll();
+      }
+    });
+
+    _refreshScroll();
+    _refreshMaxHeight();
+  }
+
+  window.GAG = window.GAG || {};
+  window.GAG.Common = new Common();
+
+  window.GAG.Common.init();
+
 })();
--- features/posts/posts.js
+++ features/posts/posts.js
@@ -1,4 +1,98 @@
 (function() {
   'use strict';
 
+  function Posts() {
+    var _url = '/posts/fetch',
+        _sort = '',
+        _start = 0,
+        _$el = {
+          list: $('.posts-list'),
+          imageTemplate: $('#post-image').html(),
+          videoTemplate: $('#post-video').html()
+        },
+        _common = window.GAG.Common;
+
+    function _fetch(callback) {
+      console.log('_fetch', {
+        start: _start,
+        sort: _sort
+      });
+
+      $.ajax({
+        type: 'GET',
+        url: _url,
+        data: {
+          start: _start,
+          sort: _sort
+        }
+      })
+        .success(function(data) {
+          if (callback) {
+            callback(data.posts);
+          }
+
+          _addPosts(data.posts);
+        })
+        .error(function() {
+          console.error(arguments);
+        });
+    }
+
+    this.init = function() {
+      // initial fetch with initial sort
+      _sort = _common.sort();
+      _fetch();
+
+      _common
+        .on('sortChanged', function(args) {
+          _sort = args.sort;
+          _start = 0;
+          _fetch(function() {
+            // restart at 0
+            _removePosts();
+          });
+        })
+        .on('infiniteScrollStart', function() {
+          _fetch();
+        });
+    };
+
+    function _addImagePost(post) {
+      var $post = $(_$el.imageTemplate);
+
+      $post.find('img').attr('src', post.image.url);
+
+      _$el.list.append($post);
+    }
+
+    function _addVideoPost(post) {
+      var $post = $(_$el.videoTemplate);
+
+      $post.find('video').attr('src', post.video.url);
+
+      _$el.list.append($post);
+    }
+
+    function _removePosts() {
+      _$el.list.empty();
+    }
+
+    function _addPosts(posts) {
+      $.each(posts, function(index, post) {
+        if (post.video) {
+          _addVideoPost(post);
+        }
+        else {
+          _addImagePost(post);
+        }
+      });
+
+      _start += posts.length;
+
+      _common.fire('infiniteScrollEnd');
+    }
+  }
+
+  new Posts().init();
+
 })();
